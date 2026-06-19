4839304a37480e0ab91c7b84ec34e2416dec1941
EvDustin <evangelrajan7@gmail.com>
2026-06-19 12:59:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+
+
 import secrets
 import string
 
 print("🔐 Password Generator")
 
-length = int(input("Enter password length: "))
+while True:
+    try:
+        length = int(input("Enter password length: "))
+
+        if length < 4:
+            print("Password length must be at least 4.")
+            continue
+
+        break
+
+    except ValueError:
+        print("Please enter a valid number.")
 
 characters = string.ascii_letters + string.digits + string.punctuation
 
